1c9b8ae4f910f6a708b0d7c2cea51f098b5586c3
22120316 <22120316@student.hcmus.edu.vn>
2025-10-29 20:47:31 +0700

    fix error

--- src/api/userApi.tsx
+++ src/api/userApi.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://userregister-be.onrender.com/";
+const API_URL = "https://userregister-be.onrender.com";
 
 export interface RegisterPayload {
   email: string;
